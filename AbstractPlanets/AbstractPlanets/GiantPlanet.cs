de883a7160958d5ca460e97209b1c92a76387469
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-23 18:02:25 -0400

    Completed the constructor for the GiantPlanet Class

--- AbstractPlanets/AbstractPlanets/GiantPlanet.cs
+++ AbstractPlanets/AbstractPlanets/GiantPlanet.cs
@@ -5,19 +5,29 @@ using System.Text;
 /*
  * Name: Shakil Hosin
  * Date: July 23rd, 2017
- * Description: This is the GiantPlanet Class
- * Version: 0.1 - Created the GiantPlanet Class
+ * Description: Took all the parameters of the planet base class and set the local variable _type to it's instance 
+ * variable in the constructor.
+ * Version: 0.2 - Set the constructor
  */
 namespace AbstractPlanets
 {
-    public class GiantPlanet
+    public class GiantPlanet : Planet , IHasMoons, IHasRings
     {
         // PRIVATE INSTANCE VARIABLES (FIELDS)
-
-        // PUBLIC PROPERTIES
-
+        private string _type;
         // CONSTRUCTORS ---------------------------------------------
-
+        /// <summary>
+        /// The GiantPlanet Class inherits from the abstract planet class
+        /// It implements the interfaces IhasMoons and IHasRings
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="type"></param>
+        public GiantPlanet(string name, double diameter, double mass, string type) :base (name, diameter, mass)
+        {
+            this._type = type;
+        }
 
         // PRIVATE METHODS
 
