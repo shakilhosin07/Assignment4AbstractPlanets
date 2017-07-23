9e663bdc9b998ac515c6b003c89a00d5d8c0869e
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-23 18:41:32 -0400

    implements the habitable method used in the subclasses.

--- AbstractPlanets/AbstractPlanets/IHabitable.cs
+++ AbstractPlanets/AbstractPlanets/IHabitable.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+/*
+ * Name: Shakil Hosin
+ * Student Number: 300922629
+ * Date: July 23rd, 2017
+ * Description: This is the IHabitable interface
+ * Version: 0.1 - Created the IHabitable interface
+ */
 namespace AbstractPlanets
 {
+    /// <summary>
+    /// implements the habitable method used in the subclasses.
+    /// </summary>
     public interface IHabitable
     {
+        bool Habitable();
     }
 }
--- AbstractPlanets/AbstractPlanets/IHasRings.cs
+++ AbstractPlanets/AbstractPlanets/IHasRings.cs
@@ -6,8 +6,8 @@ using System.Text;
  * Name: Shakil Hosin
  * Student Number: 300922629
  * Date: July 23rd, 2017
- * Description: This is the IHasMoons interface
- * Version: 0.1 - Created the IHasMoons interface
+ * Description: This is the IHasRings interface
+ * Version: 0.1 - Created the IHasRings interface
  */
 namespace AbstractPlanets
 {
