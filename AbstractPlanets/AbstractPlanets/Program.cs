a67f88912a8c739e4bdd599f50c83046681e9ffb
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-07-23 20:06:15 -0400

    Finalized the output of the driver class

--- AbstractPlanets/AbstractPlanets/Program.cs
+++ AbstractPlanets/AbstractPlanets/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Student Number: 300922629
  * Date: July 23rd, 2017
  * Description: This is the Driver class
- * Version: 0.3 - Implementing the interfaces we created for the planets.
+ * Version: 1.0 - Finalized the output of the driver class.
  */
 namespace AbstractPlanets
 {
@@ -16,25 +16,36 @@ namespace AbstractPlanets
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("===========================================================\n");
+            Console.WriteLine("Shakil Hosin -- 300922629 -- Assignment 4 -- Abstract Planets");
+            Console.WriteLine("===========================================================\n");
+
             //Giant Planet Object
             GiantPlanet Shaktopia = new GiantPlanet("Shaktopia", 200000, 10000000020, "gas");
             Shaktopia.OrbitalPeriod = 95;
             Shaktopia.RotationPeriod = 762;
             Shaktopia.MoonCount = 5;
+            Console.WriteLine("STATS");
+            Console.WriteLine(Shaktopia);
             Console.WriteLine("Has Moons?");
             Console.WriteLine(Shaktopia.HasMoons() ? "Yes" : "No");
             Console.WriteLine("Has Rings?");
             Console.WriteLine(Shaktopia.HasRings() ? "Yes" : "No");
-            Console.WriteLine();
-            Console.WriteLine("STATS");
-            Console.WriteLine(Shaktopia);
+            Console.WriteLine("===========================================================");
 
             //Terrestrial Planet Object
-            TerrestrialPlanet Torontoplo = new TerrestrialPlanet("earth", 28060708, 912827633674746434.393, true);
+            TerrestrialPlanet Torontoplo = new TerrestrialPlanet("Torontoplo", 28060708, 912827633674746434.393, true);
             Torontoplo.OrbitalPeriod = 18;
             Torontoplo.RotationPeriod = 190;
             Torontoplo.MoonCount = 0;
+            Console.WriteLine();
+            Console.WriteLine("STATS");
             Console.WriteLine(Torontoplo);
+            Console.WriteLine("Has Moons?");
+            Console.WriteLine(Torontoplo.HasMoons() ? "Yes" : "No");
+            Console.WriteLine("Habitable?");
+            Console.WriteLine(Torontoplo.Habitable() ? "Yes" : "No");
+            Console.WriteLine("===========================================================");
             WaitForKey();
 
         }
